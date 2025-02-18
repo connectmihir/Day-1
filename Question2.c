82243ae91761749092b8f9aad9059e62ff7521ca
Mihir Ranjan <connectmihirr@gmail.com>
2025-02-18 11:30:03 +0530

    Add files via upload
    This is my day of practice more to improve myself.

---
+++ Question2.c
@@ -0,0 +1,18 @@
+
+//
+
+#include <stdio.h>
+
+int main()
+{
+    // Initializing the required variables
+
+    int ;
+
+    // Taking inputs from the user
+
+    printf("Enter the Length of Rectangle:");
+    scanf("%d",& l);
+    
+    return 0;
+}
